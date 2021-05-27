ff9a50acbe3943c93a7874b5ec8138f93f4ac021
ABHIJEET KUMAR <abhijeet8825>
2021-05-27 19:09:10 +0530

    Initial commit

---
+++ User_define_module.py
@@ -0,0 +1,43 @@
+import math
+def add(x,y):
+    return x+y
+
+
+def sub(x,y):
+    return x-y
+
+def mul(x,y):
+    return x*y
+
+def div(x,y):
+    return x/y
+
+
+
+def pow(x,y):
+    return x**y
+
+def square(x):
+    return x*x
+
+
+def fact(x):
+    return math.factorial(x)
+
+
+
+def even(c):
+    if c%2==0:
+        print("The number is even")
+
+
+    else:
+        print("The number is odd")
+
+
+def lcm (x,y):
+    return math.lcm(x,y)
+
+
+def gcd(x,y):
+    return math.gcd(x,y)
